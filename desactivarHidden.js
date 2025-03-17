3f9c973703aae5314354df765fd02c6c85e638c1
kevin <kevinenriquep26@gmail.com>
2025-03-17 11:07:47 -0300

    de nuevo el script

---
+++ desactivarHidden.js
@@ -0,0 +1,9 @@
+window.quitarClaseHidden = function(id) {
+    const  elemento = document.getElementById(id)
+    if(elemento) {
+        elemento.classList.remove('hidden')
+        console.log(`Clase 'hidden' eliminada de ${id}`)
+    } else {
+        console.log(`No se encontro el elemento con ID: ${id}`)     
+    }
+}
